f1a9694d045752ef065a630a5cca2a527646f303
Silver Bullet <traingheoktt@gmail.com>
2023-01-02 14:50:16 +0700

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Navbar from "./components/Navbar";
-import Todos from "./components/Todos";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Todos />
     </div>
   );
 }
